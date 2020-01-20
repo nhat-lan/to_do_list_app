5778c55fff30428d8a40310a060761a7fdb696ea
Lan Le Tu <lan.letu96@gmail.com>
2020-01-19 20:27:18 -0500

    Fix list path, tested endpoints

--- backend/routes/list/list.route.js
+++ backend/routes/list/list.route.js
@@ -2,19 +2,21 @@ const router = require('express').Router();
 const listController = require('../../controllers/list.controller.js');
 
 router.route('/list')
-    // GET list by ID
-    .get((req, res) => {
-        listController.getListByid(req.params.listId, res);
-    })
     // POST create a new list
     .post((req, res) => {
         listController.createNewList(req.body, res);
+    });
+
+router.route('/list/:listId')    
+    // GET list by ID
+    .get((req, res) => {
+        listController.getListByid(req.params.listId, res);
     })
     // PUT update a list by ID
     .put((req, res) => {
         listController.updateListData(req.body, req.params.listId, res);
     })
-    // DELETE a lit
+    // DELETE a lit by ID
     .delete((req, res) => {
         listController.deleteListByid(req.params.listId, res);
     });
