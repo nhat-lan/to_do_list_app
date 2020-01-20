ae1b5a205929cee74545d5605c5c0371d610ac82
Lan Le Tu <lan.letu96@gmail.com>
2020-01-19 20:06:04 -0500

    Add list route

---
+++ backend/routes/list/list.route.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const listController = require('../../controllers/list.controller.js');
+
+router.route('/tests')
+    // GET list by ID
+    .get((req, res) => {
+        listController.getListByid(req.params.listId, res);
+    })
+    // POST create a new list
+    .post((req, res) => {
+        listController.createNewList(req.body, res);
+    })
+    // PUT update a list by ID
+    .put((req, res) => {
+        listController.updateListData(req.body, req.params.listId, res);
+    })
+    // DELETE a lit
+    .delete((req, res) => {
+        listController.deleteListByid(req.params.listId, res);
+    });
+
+module.exports = router;
