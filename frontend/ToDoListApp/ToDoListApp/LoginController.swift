8ee2811b94b1985994be5f2db127b207b7e025da
Lan Le Tu <lan.letu96@gmail.com>
2020-01-21 14:43:53 -0500

    Controller login works

--- frontend/ToDoListApp/ToDoListApp/LoginController.swift
+++ frontend/ToDoListApp/ToDoListApp/LoginController.swift
@@ -20,19 +20,50 @@ class LoginController: UIViewController {
     
     @IBOutlet weak var usernameTextField: UITextField!
     
+    
+    
     @IBOutlet weak var passwordTextField: UITextField!
+    
     
     @IBOutlet weak var signinButton: UIButton!
     
     
-    @IBAction func signinButtonAction(_ sender: UIButton) {
+    
+    @IBAction func singinButtonAction(_ sender: UIButton) {
         // check in database
+        let passText = self.passwordTextField.text
+        let get = Api(path: "accounts/" + self.usernameTextField.text!, apiMethod: "GET", body: [:])
+        _ = get.apiCall(callback: { data in
+
+            if (data == nil) {
+                DispatchQueue.main.async {
+                    self.alert(controllerTitle: "User doesn't exist", message: "Check username")
+                }
+            } else if (String(describing: data!["password"]!) != passText) {
+                DispatchQueue.main.async {
+                    self.alert(controllerTitle: "Wrong credentials", message: "Username and password doesn't match")
+                }
+            } else {
+                DispatchQueue.main.async {
+                    let storyboard : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+
+                    let nextView = storyboard.instantiateViewController(withIdentifier: "welcome") as! ViewController
+
+                    self.navigationController?.pushViewController(nextView, animated:true)
+                }
+
+            }
+        })
     }
     
-    
-    
+    func alert(controllerTitle: String, message: String) {
 
-    @IBAction func signinAction(_ sender: UIButton) {
+        let alertController = UIAlertController(title: controllerTitle, message: message, preferredStyle: .alert)
         
+        let alertAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        
+        alertController.addAction(alertAction)
+        
+        self.present(alertController, animated: true, completion: nil)
     }
 }
